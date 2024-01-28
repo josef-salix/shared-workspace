14e909d100b2dd7b509aaacc9bdc2f287cc7d2c6
Cicin2837512 <CicinGithub@skiff.com>
2024-01-28 09:18:06 +0100

    added cmake folder

---
+++ cmake/build/CMakeFiles/hello_world_program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_program.dir/main.c.o"
+  "CMakeFiles/hello_world_program.dir/main.c.o.d"
+  "hello_world_program"
+  "hello_world_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_world_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
