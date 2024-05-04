743f65e9f94903b65c621606a901cf917ce4275f
josef-salix <josef.salix@skiff.com>
2024-05-04 11:13:24 +0200

    edit index.css and index.js

---
+++ Javascript/BroCodeJavascript/Video1/index.js
@@ -0,0 +1,8 @@
+console.log("hello javascript world");
+console.log("I like pizza");
+
+// window.alert("I like pizza");
+
+document.getElementById("myH1").textContent = "I like pizza";
+document.getElementById("myP").textContent = "This is a first paragraph in this folder and it is too short."
+
